d05fa4b6ab277b7f7e683edbb46b05b7693e027d
alissasalazar <salazargamarraa@gmail.com>
2023-01-24 23:17:31 -0500

    feat: validator and style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+//codigo creado por laboratoria para validación//
+
+export {validate};
+
+function validate() {
+  const cardnumber = document.getElementById("cardnumber");
+  const result = document.getElementById("result");
+  let message="";
+  
+  if (validator.isValid (cardnumber.value)) {
+    cardnumber.value=validator.maskify(cardnumber.value); 
+    message= "Tu número de tarjeta de credito es válido";
+  }
+  // Show success in div#result
+  else {
+    message="Tu número de tarjeta de credito no es válido"; 
+  }
+  // Show error message in div#result
+  result.textContent=message;
+}
+document.getElementById("validar").addEventListener("click",validate);
